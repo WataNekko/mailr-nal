cd4eab4247d0e18ebf6184d1a715abb4485d80c8
WataNekko <88185666+WataNekko@users.noreply.github.com>
2024-09-03 20:04:45 +0700

    Add Command trait and move EHLO logic

--- src/smtp/commands.rs
+++ src/smtp/commands.rs
@@ -1,3 +1,23 @@
+use embedded_nal::TcpClientStack;
+use enumset::EnumSet;
+
+use super::{
+    extensions::{EhloInfo, SmtpExtension},
+    response::{ReplyLine, ResponseParser},
+    ConnectError,
+};
+use crate::io::{BufWriter, TcpStream, WithBuf};
+
+pub trait Command<T>
+where
+    T: TcpClientStack,
+{
+    type Output;
+    type Error;
+
+    fn execute(self, stream: &mut WithBuf<TcpStream<T>>) -> Result<Self::Output, Self::Error>;
+}
+
 /// Domain or address literal that identifies the client (https://www.rfc-editor.org/rfc/rfc5321#section-4.1.1.1)
 #[repr(transparent)]
 pub struct ClientId<'a>(&'a str);
@@ -26,8 +46,63 @@ impl core::fmt::Display for ClientId<'_> {
 
 pub struct Ehlo<'a>(pub(crate) ClientId<'a>);
 
-impl core::fmt::Display for Ehlo<'_> {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "EHLO {}\r\n", self.0)
+impl<T: TcpClientStack> Command<T> for Ehlo<'_> {
+    type Output = EhloInfo;
+    type Error = ConnectError<T::Error>;
+
+    fn execute(self, stream: &mut WithBuf<TcpStream<T>>) -> Result<Self::Output, Self::Error> {
+        let Self(client_id) = self;
+
+        {
+            let mut stream = BufWriter::from(&mut *stream);
+            write!(stream, "EHLO {}\r\n", client_id)?;
+        }
+
+        let mut response = ResponseParser::new(stream);
+
+        {
+            // skip first greeting line
+            let ReplyLine {
+                code: b"250",
+                has_next: true,
+                ..
+            } = response.next_line()?
+            else {
+                return Err(ConnectError::UnexpectedResponse);
+            };
+        }
+
+        // process extensions
+        let mut extensions = EnumSet::new();
+
+        loop {
+            let ReplyLine {
+                code: b"250",
+                text,
+                has_next,
+            } = response.next_line()?
+            else {
+                return Err(ConnectError::UnexpectedResponse);
+            };
+
+            let mut words = text.split(' ');
+
+            let ext = words.next().ok_or(ConnectError::UnexpectedResponse)?;
+
+            extensions |= match ext {
+                "AUTH" => EnumSet::from_iter(words.map(|mech| match mech {
+                    "PLAIN" => SmtpExtension::AuthPlain.into(),
+                    "LOGIN" => SmtpExtension::AuthLogin.into(),
+                    _ => EnumSet::empty(),
+                })),
+                _ => EnumSet::empty(),
+            };
+
+            if !has_next {
+                break;
+            }
+        }
+
+        Ok(EhloInfo { extensions })
     }
 }
--- src/smtp/mod.rs
+++ src/smtp/mod.rs
@@ -4,15 +4,14 @@ mod response;
 
 use core::fmt::Debug;
 use embedded_nal::{nb::block, AddrType, Dns, SocketAddr, TcpClientStack, TcpError};
-use enumset::EnumSet;
 
 pub use self::commands::ClientId;
-use self::commands::Ehlo;
-use self::extensions::{EhloInfo, SmtpExtension};
-use self::response::{ReplyLine, ResponseError, ResponseParser};
+use self::commands::{Command, Ehlo};
+use self::extensions::EhloInfo;
+use self::response::{ResponseError, ResponseParser};
 use crate::{
     auth::Credential,
-    io::{BufWriter, TcpStream, WithBuf},
+    io::{TcpStream, WithBuf},
 };
 
 pub struct SmtpClient;
@@ -73,7 +72,7 @@ where
         Self::server_greeting(&mut stream)?;
 
         let client_id = client_id.unwrap_or(ClientId::localhost());
-        let ehlo_info = Self::ehlo(&mut stream, client_id)?;
+        let ehlo_info = Ehlo(client_id).execute(&mut stream)?;
 
         Ok(SmtpClientSession { stream, ehlo_info })
     }
@@ -97,63 +96,6 @@ where
     fn server_greeting(stream: &mut WithBuf<TcpStream<T>>) -> Result<(), ConnectError<T::Error>> {
         ResponseParser::new(stream).expect_code(b"220")?;
         Ok(())
-    }
-
-    fn ehlo(
-        stream: &mut WithBuf<TcpStream<T>>,
-        client_id: ClientId,
-    ) -> Result<EhloInfo, ConnectError<T::Error>> {
-        {
-            let mut stream = BufWriter::from(&mut *stream);
-            write!(stream, "{}", Ehlo(client_id))?;
-        }
-
-        let mut response = ResponseParser::new(stream);
-
-        {
-            // skip first greeting line
-            let ReplyLine {
-                code: b"250",
-                has_next: true,
-                ..
-            } = response.next_line()?
-            else {
-                return Err(ConnectError::UnexpectedResponse);
-            };
-        }
-
-        // process extensions
-        let mut extensions = EnumSet::new();
-
-        loop {
-            let ReplyLine {
-                code: b"250",
-                text,
-                has_next,
-            } = response.next_line()?
-            else {
-                return Err(ConnectError::UnexpectedResponse);
-            };
-
-            let mut words = text.split(' ');
-
-            let ext = words.next().ok_or(ConnectError::UnexpectedResponse)?;
-
-            extensions |= match ext {
-                "AUTH" => EnumSet::from_iter(words.map(|mech| match mech {
-                    "PLAIN" => SmtpExtension::AuthPlain.into(),
-                    "LOGIN" => SmtpExtension::AuthLogin.into(),
-                    _ => EnumSet::empty(),
-                })),
-                _ => EnumSet::empty(),
-            };
-
-            if !has_next {
-                break;
-            }
-        }
-
-        Ok(EhloInfo { extensions })
     }
 }
 
