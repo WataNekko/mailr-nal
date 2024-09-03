730b60a1d1aef74e597b136bc952a9ffb10aaa19
WataNekko <88185666+WataNekko@users.noreply.github.com>
2024-09-04 01:25:16 +0700

    Add tests for message, send and close

--- tests/smtp_std_embedded_nal.rs
+++ tests/smtp_std_embedded_nal.rs
@@ -70,3 +70,119 @@ mod connect {
             .expect("connected to hostname");
     }
 }
+
+#[cfg(test)]
+mod send {
+    use mailr_nal::{
+        message::{Envelope, Mail, Mailbox},
+        smtp::SmtpClient,
+    };
+    use test_common::TestContext;
+
+    #[test]
+    fn mail_message() {
+        let _ = Mail::new()
+            .from("Smith@bar.com")
+            .to(&["Jones@foo.com"])
+            .cc(&["Green@foo.com"])
+            .bcc(&["Brown@foo.com"])
+            .subject("Test mail")
+            .body("Blah blah blah...\r\n..etc. etc. etc.");
+    }
+
+    #[cfg(todo)]
+    #[test]
+    fn send_mail() {
+        let TestContext { plain_port, .. } = TestContext::setup();
+
+        let mut stack = std_embedded_nal::Stack;
+        let mut buf = [0; 1024];
+
+        let mut client = SmtpClient::new(&mut stack, &mut buf)
+            .connect(([127, 0, 0, 1], plain_port))
+            .unwrap();
+
+        let mail = Mail::new()
+            .from("Smith@bar.com")
+            .to(&["Jones@foo.com"])
+            .cc(&["Green@foo.com"])
+            .bcc(&["Brown@foo.com"])
+            .subject("Test mail")
+            .body("Blah blah blah...\r\n..etc. etc. etc.");
+
+        client.send(&mail).expect("sent first message successfully");
+    }
+
+    #[cfg(todo)]
+    #[test]
+    fn send_multiple_mails() {
+        let TestContext { plain_port, .. } = TestContext::setup();
+
+        let mut stack = std_embedded_nal::Stack;
+        let mut buf = [0; 1024];
+
+        let mut client = SmtpClient::new(&mut stack, &mut buf)
+            .connect(([127, 0, 0, 1], plain_port))
+            .unwrap();
+
+        let mail = Mail::new()
+            .from("Smith@bar.com")
+            .to(&["Jones@foo.com"])
+            .cc(&["Green@foo.com"])
+            .bcc(&["Brown@foo.com"])
+            .subject("Test mail")
+            .body("Blah blah blah...\r\n..etc. etc. etc.");
+
+        client.send(&mail).expect("sent first message successfully");
+
+        let mail = mail.subject("Test mail 2");
+
+        client
+            .send(&mail)
+            .expect("sent second message successfully");
+    }
+
+    #[cfg(todo)]
+    #[test]
+    #[ignore]
+    fn send_raw() {
+        let TestContext { plain_port, .. } = TestContext::setup();
+
+        let mut stack = std_embedded_nal::Stack;
+        let mut buf = [0; 1024];
+
+        let mut client = SmtpClient::new(&mut stack, &mut buf)
+            .connect(([127, 0, 0, 1], plain_port))
+            .unwrap();
+
+        let envelope = Envelope::new("Smith@bar.com", "Jones@foo.com");
+        let raw_message = concat!(
+            "From:<Smith@bar.com>\r\n",
+            "To:<Jones@foo.com>\r\n",
+            "Subject: Test mail\r\n",
+            "\r\n",
+            "Blah blah blah...\r\n",
+            "..etc. etc. etc."
+        );
+
+        client
+            .send_raw(envelope, raw_message)
+            .expect("sent successfully");
+    }
+
+    #[cfg(todo)]
+    #[test]
+    #[ignore]
+    fn close() {
+        let TestContext { plain_port, .. } = TestContext::setup();
+
+        let mut stack = std_embedded_nal::Stack;
+        let mut buf = [0; 1024];
+
+        let client = SmtpClient::new(&mut stack, &mut buf)
+            .connect(([127, 0, 0, 1], plain_port))
+            .unwrap();
+
+        client.close().expect("close successfully");
+    }
+}
